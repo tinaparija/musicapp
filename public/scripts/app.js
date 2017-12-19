30ae94b299cfe3036c3247a03b82c135eadf8183
Eva <evaliisagalen@gmail.com>
2017-12-19 13:36:59 -0800

    created basic html/css structure

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,31 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
   console.log("Document loaded");
 
-});
+  //on page load, GET all moods
+  // $.get('/api/moods').success(function (moods) {
+  //   moods.forEach(function(mood) {
+  //     renderMood(mood);
+  //   });
+  // });
+
+  $(".mood").on('click', function(e) { //changed mood color on click
+    $(this).css("background", "orange");
+  })
+
+
+
+
+
+
+
+
+function renderMood(mood) {
+
+};
+
+function onError() {
+
+};
+
+
+}); //document ready end
