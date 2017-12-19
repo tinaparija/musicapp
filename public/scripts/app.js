7349bcd9572058d70df010792a0651ee258291aa
Eva <evaliisagalen@gmail.com>
2017-12-19 14:27:08 -0800

    added ajax function to get all moods data and started necessary success function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,16 +3,18 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
   console.log("Document loaded");
 
-  //on page load, GET all moods
-  // $.get('/api/moods').success(function (moods) {
-  //   moods.forEach(function(mood) {
-  //     renderMood(mood);
-  //   });
-  // });
+  $.ajax({
+    method: 'GET',
+    url: '/api/moods',
+    success: onGetSuccess,
+    error: onError
+  });
 
-  $(".mood").on('click', function(e) { //changed mood color on click
+  $(".mood").on('click', function(e) {
     $(this).css("background", "orange");
-  })
+    //remove previous mood
+    displayMood(mood);
+  });
 
 
 
@@ -22,11 +24,23 @@ $(document).ready(function(){
 
 
 function renderMood(mood) {
-
+// add in correct path to color hex and mood name to display the buttons
+  let moodSelections = `<div class="row mood-selection" style="background-color:${getColorHex}"><div class="col-1 mood mood-block"><span class="mood-title">${dataNameHere}</span></div>`
+  $(".mood-selection").append(moodSelections);
 };
 
-function onError() {
+function displayMood(mood) {
+  //
+};
 
+function onGetSuccess(moodsData) {
+  moodsData.forEach(function(mood) {
+    renderMood(mood);
+  });
+};
+
+function onError(err) {
+  console.log("There was an error ", err);
 };
 
 
