b7011adbaefc35c4d2400a8cef6e81ca4ea96860
Eva <evaliisagalen@gmail.com>
2017-12-19 09:35:39 -0800

    basic setup

--- server.js
+++ server.js
@@ -20,3 +20,25 @@ app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
 });
 
+app.get('/api', function apiIndex(req, res) {
+  // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
+  // It would be seriously overkill to save any of this to your database.
+  // But you should change almost every line of this response.
+  res.json({
+    message: "Welcome to my personal api address book! Here's what you need to know!",
+    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
+    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    endpoints: [
+      {method: "GET", path: "/api", description: "Describes all available endpoints"},
+      {method: "GET", path: "/api/contacts", description: "See all my contacts"}, // CHANGE ME
+      {method: "POST", path: "/api/contacts", description: "Create a new contact"} // CHANGE ME
+    ]
+  })
+});
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on the port that Heroku prescribes (process.env.PORT) OR port 3000
+ app.listen(process.env.PORT || 3000)
