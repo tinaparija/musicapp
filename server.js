74a3afd94f80704e130984fca4e877231c414972
tinaparija <tinaparija@gmail.com>
2017-12-19 09:36:22 -0800

    endpoint updates

--- server.js
+++ server.js
@@ -20,3 +20,48 @@ app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
 });
 
+/*
+ * JSON API Endpoints
+ */
+
+app.get('/api', function (req, res) {
+  res.json({
+    documentationUrl: "ADD URL", 
+    baseUrl: "ADD URL", // 
+    endpoints: [{
+          method: "GET",
+          path: "/api",
+          description: "describes all endpoints"
+        },
+        {
+          method: "GET",
+          path: "/api/moods",
+          description: "displays all moods"
+        },
+        {
+          method: "GET",
+          path: "/api/moods/:id",
+          description: "displays a single mood by id"
+        },
+        {
+          method: "GET",
+          path: "api/songs",
+          description: "displays all songs by id"
+        },
+        {
+          method: "POST",
+          path: "/api/cities",
+          description: "adds new cities"
+        }]
+      }); 
+  }); 
+
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on the port that Heroku prescribes (process.env.PORT) OR port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is up and running on http://localhost:3000/')
+})
