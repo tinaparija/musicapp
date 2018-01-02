9d0e193c99c5eea757964b652f4f1caeb18353c2
Esther Weon <esther.s.weon@gmail.com>
2018-01-02 11:54:13 -0800

    PR comments

--- calm-gorge-34925/models/index.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/musicapp", {useMongoClient: true});
-mongoose.Promise = global.Promise; 
-
-
-module.exports.Song = require("./song.js");
-module.exports.Mood = require("./mood.js");
